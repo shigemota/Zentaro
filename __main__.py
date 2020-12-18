a6f07f39375d9d047988a2b13aff82f4ec236142
TheNeonHaze <43961871+TheNeonHaze@users.noreply.github.com>
2020-12-18 23:31:34 +0600

    added __main__.py

---
+++ __main__.py
@@ -0,0 +1,8 @@
+from views import *
+from app import app
+
+
+if __name__ == '__main__':
+    db.bind(**app.config['PONY'])
+    db.generate_mapping(create_tables=True)
+    app.run(port=8080, host='127.0.0.1')
